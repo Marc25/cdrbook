43124037b13d2922c8250ff005b5d9a825714f49
Marc25 <mad@Joses-MacBook-Pro.local>
2024-03-03 00:18:05 +0100

    Incluye 2 modelo y adecuacon CDR Cap16

--- CDRC16.R
+++ CDRC16.R
@@ -32,4 +32,26 @@ plot(dhos_rp)
 
 # Viendo el coef de diag1 el numero medio de dias en el hosp es 3 veces mayor para cl
 # con diag1 que resp al diag0
+# sexo1 : el coef 1.23 indica que el sexo1 hombre incrementa el numeero de dias mas que una mujer
+
+# Ajuste02 ----
+dhos_rp2 <- glm(dhosp ~ diag + sexo+edad, family = "poisson", data=dtClev)
+summary(dhos_rp2)
+
+# Adecuación ----
+# En los ajustes reg_pois y reg_pois2 hay información sobre la null y la residual deviance, con las que se puede realizar el contraste de comparación de modelos (el simple frente al elaborado) mencionado en la Sec. 16.4.2.
+# Contratamos entoncs el modelo frente al modelo nulo la media de la var
+pchisq(dhos_rp$deviance, dhos_rp$df.residual, lower.tail = F)
+pchisq(dhos_rp2$deviance, dhos_rp$df.residual, lower.tail = F)
+# Al ser los p–valores superiores a 0,05, 
+# se puede considerar que ambos modelos explican “mejor” que el modelo nulo, 
+# el que únicamente contiene la constante y el término aleatorio.
+# 
+# Adecuaci'on del Modelo 
+La diferencia entre la deviance de un modelo más elaborado y el simple se distribuye como una 
+χ
+2
+con tantos grados de libertad como restricciones impuestas a los parámetros, lo que permite contrastar cuál de los dos modelos ajusta mejor los datos. P–valores bajos indican que el modelo ajustado es inadecuado, debiendo investigarse otras posibles variables predictoras, si se incumple la hipótesis de linealidad o existe sobredispersión (por ejemplo, por exceso de ceros).
+
+
 
